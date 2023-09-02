c35c9743a43179ddabd30de5b03d9d9b7884a746
MD. ASAD-AL-ADIL <adilasadals@gmail.com>
2023-09-03 00:42:25 +0600

    final_project

--- list/urls.py
+++ list/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
+    path('',views.home,name='homepage'),
     path('home/',views.home,name='homepage'),
     path('complete/<int:id>/',views.complete_task,name='completepage'),
     path('completed/',views.completed_tasks,name='completedpage'),
